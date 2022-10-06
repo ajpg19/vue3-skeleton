2ee944a9b38b8ade139b5474ab91f889efe06269
Albertj PG <ajpg19@gmail.com>
2022-10-06 16:02:51 +0200

    add router4

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue'
+import {ref} from 'vue'
 
 defineProps({
   msg: String
@@ -9,7 +9,22 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="./../assets/vite.svg" class="logo" alt="Vite logo"/>
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./../assets/vue.svg" class="logo vue" alt="Vue logo"/>
+    </a>
+    <a href="https://pinia.vuejs.org/" target="_blank">
+      <img src="https://pinia.vuejs.org/logo.svg" class="logo vue" alt="Pinia logo"/>
+    </a>
+    <a href="https://kazupon.github.io/vue-i18n/" target="_blank">
+      <img src="https://kazupon.github.io/vue-i18n/vue-i18n-logo.png" class="logo vue" alt="Vue i18n logo"/>
+    </a>
+  </div>
+
+  <h2>{{ msg }}</h2>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
@@ -22,13 +37,8 @@ const count = ref(0)
   <p>
     Check out
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
+    >create-vue</a
     >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
@@ -36,5 +46,16 @@ const count = ref(0)
 <style scoped>
 .read-the-docs {
   color: #888;
+}
+.logo {
+  height: 4em;
+  padding: 1em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
