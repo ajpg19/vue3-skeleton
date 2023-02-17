41df1ee9602c2b466da6bc13fb99d560b2a2a22c
Albertj PG <ajpg19@gmail.com>
2023-02-17 09:41:52 +0100

    updating code skeleton struct

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-//Define a index components
-export {default as ApplicationFooter} from './ApplicationFooter.vue'
-export {default as ApplicationHeader} from './ApplicationHeader.vue'
-
-export {default as HelloWorld} from './HelloWorld.vue'
-export {default as LogoMain} from './LogoMain.vue'
