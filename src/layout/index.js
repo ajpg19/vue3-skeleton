1086d3235e4924f8d73e29ede11af4b19dbbdc79
Albertj PG <ajpg19@gmail.com>
2023-02-17 09:41:52 +0100

    updating code skeleton struct

--- src/layout/index.js
+++
@@ -1,4 +0,0 @@
-//Define index layout
-export {default as LayoutAuth} from './LayoutAuth.vue'
-export {default as LayoutMain} from './LayoutMain.vue'
-
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
 import { defineConfig } from 'vite'
-import vueI18n from '@intlify/vite-plugin-vue-i18n' //internationalization
-import {dirname, resolve} from 'node:path'
-import {fileURLToPath, URL} from 'url'//Alias to friendly urls
-
 import vue from '@vitejs/plugin-vue'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-      vue(),
-      vueI18n({
-        include: resolve(dirname(fileURLToPath(import.meta.url)), './src/locales/**')//messages
-      }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))//define de alias
-    }
-  }
+  plugins: [vue()],
 })
